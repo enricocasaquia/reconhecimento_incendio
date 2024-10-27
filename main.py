d5e49edeb934ec9a8716bdc325876366885cae65
Vinicius Feliciano <viniciuspradofeliciano@gmail.com>
2024-10-27 17:50:28 -0300

    Agora subindo as coisas na main para poder exlcuir a master

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Leitura e conversão da imagem
 imagem = cv2.imread('samples/incendio4.png')
 imagem_rgb = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)  # Conversão para RGB
@@ -12,7 +13,7 @@ imagem_hsv = cv2.cvtColor(imagem_suave, cv2.COLOR_RGB2HSV)
 
 # Intervalos de cor HSV para detecção de fogo
 lower_fire = np.array([0, 100, 150])  # Ajustar o valor conforme necessário
-upper_fire = np.array([25, 255, 255])
+upper_fire = np.array([10, 245, 255])
 
 # Criação da máscara de detecção de fogo
 mascara_fogo = cv2.inRange(imagem_hsv, lower_fire, upper_fire)
@@ -23,7 +24,7 @@ mascara_fogo = cv2.morphologyEx(mascara_fogo, cv2.MORPH_CLOSE, kernel)
 mascara_fogo = cv2.morphologyEx(mascara_fogo, cv2.MORPH_OPEN, kernel)
 
 # Resultado da aplicação da máscara na imagem original
-result = cv2.bitwise_and(imagem_rgb, imagem_rgb, mask=mascara_fogo)
+result = cv2.bitwise_and(imagem, imagem, mask=mascara_fogo)
 
 # Detecção de contornos
 contours, _ = cv2.findContours(mascara_fogo, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -31,9 +32,16 @@ contours, _ = cv2.findContours(mascara_fogo, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIM
 # Desenho de retângulos ao redor dos focos detectados
 for contour in contours:
     area = cv2.contourArea(contour)
-    if 1000 < area < 999999:  # Definindo limites de área mínima e máxima
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(imagem_rgb, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Cor verde para os focos detectados
+    if 0 < area < 999999:  # Definindo limites de área mínima e máxima
+        # Aproximação do contorno para verificar irregularidade
+        perimetro = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.02 * perimetro, True)
+
+        # Verifica se o contorno é "irregular" (mais de 4 vértices)
+        if len(approx) > 4:
+            x, y, w, h = cv2.boundingRect(contour)
+            #cv2.rectangle(imagem, (x, y), (x + w, y + h), (0, 254, 0), 2)  # Cor verde
+            cv2.drawContours(imagem, [contour], -1, (0, 254, 0), 2)
 
 # Exibição das imagens
 cv2.imshow('Imagem Original', imagem)
